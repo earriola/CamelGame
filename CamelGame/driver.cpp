8c28ed8872291af7d9ce97fcaaec266bf945ab95
earriola <earriola5@stu.ctcd.edu>
2017-05-01 10:01:26 -0500

    Completed game

--- CamelGame/driver.cpp
+++ CamelGame/driver.cpp
@@ -19,6 +19,10 @@ void aheadModerate(int &thirst, int &camelTiredness, int &miles, int &nativesDis
 void aheadFull(int &thirst, int &camelTiredness, int &miles, int &nativesDistance);
 void stopForNight(int &camelTiredness);
 void statusCheck(int canteenDrinks, int miles, int nativesDistance);
+void foundOasis(int &canteenDrinks, int &thirst, int &camelTiredness);
+void quit(bool &done, char userChoice);
+void alerts(int miles, int thirst, int camelTiredness, bool &done, int nativesDistance);
+void actions(char userChoice, int &thirst, int &camelTiredness, int &miles, int &nativesDistance, bool &done, int &canteenDrinks);
 
 int main()
 {
@@ -35,12 +39,13 @@ int main()
 		
 	while (!done)
 	{
-		userChoice = displayMenu();
+		clearScreen();
 		
-		if ((userChoice == 'Q') || (userChoice == 'q'))
-		{
-			done = true;
-		}
+		userChoice = displayMenu();
+		foundOasis(canteenDrinks, thirst, camelTiredness);
+		
+		actions(userChoice, thirst, camelTiredness, miles, nativesDistance, done, canteenDrinks);
+		alerts(miles, thirst, camelTiredness, done, nativesDistance);
 	}
 	
 	return 0;
@@ -69,7 +74,8 @@ char displayMenu()
 	
 	while (!isValid(userChoice))
 	{
-		cout << "ERROR! Enter valid choice from the menu." << endl;
+		cout << endl;
+		cout << "ERROR! Enter valid choice from the menu." << endl << endl;
 		userChoice = displayMenu();
 	}
 	
@@ -133,7 +139,7 @@ void drinkCanteen(int &canteenDrinks, int &thirst)
 void aheadModerate(int &thirst, int &camelTiredness, int &miles, int &nativesDistance)
 {
 	miles = miles + (rand() % 12 + 5);
-	cout << miles << "traveled." << endl;
+	cout << miles << " miles traveled." << endl;
 	
 	thirst = thirst + 1;
 	
@@ -145,7 +151,7 @@ void aheadModerate(int &thirst, int &camelTiredness, int &miles, int &nativesDis
 void aheadFull(int &thirst, int &camelTiredness, int &miles, int &nativesDistance)
 {
 	miles = miles + (rand() % 20 + 10);
-	cout << miles << "traveled." << endl;
+	cout << miles << " miles traveled." << endl;
 	
 	thirst = thirst + 1;
 	
@@ -165,3 +171,133 @@ void statusCheck(int canteenDrinks, int miles, int nativesDistance)
 	cout << "Drinks in canteen: " << canteenDrinks << endl;
 	cout << "The natives are " << nativesDistance << " miles behind you." << endl;
 }// end statusCheck
+
+void foundOasis(int &canteenDrinks, int &thirst, int &camelTiredness)
+{
+	int num = 7;
+	int randNum;
+	
+	randNum = rand() % 20 + 1;
+	
+	if (randNum == num)
+	{
+		cout << "You have found an oasis!!" << endl;
+		canteenDrinks = 10;
+		thirst = 0;
+		camelTiredness = 0;
+	}
+}// end foundOasis
+
+void quit(bool &done, char choice)
+{
+	if ((choice = 'Q') || (choice = 'q'))
+	{
+		done = true;
+	}
+}// end quit
+
+void alerts(int miles, int thirst, int camelTiredness, bool &done, int nativesDistance)
+{
+	if (thirst > 6)
+	{
+		cout << "You died of thirst!" << endl;
+		done = true;
+		return;
+	}
+	else if ((thirst > 4) && (thirst <= 6))
+	{
+		cout << "You are thirsty!" << endl;
+	}
+	if (camelTiredness > 8)
+	{
+		cout << "Your camel is dead!" << endl;
+		done = true;
+		return;
+	}
+	else if ((camelTiredness > 5) && (camelTiredness <= 8))
+	{
+		cout << "Your camel is getting tired!" << endl;
+	}
+	if (nativesDistance >= miles)
+	{
+		cout << "The natives have caught you!" << endl;
+		done = true;
+		return;
+	}
+	else if (nativesDistance > (miles - 15))
+	{
+		cout << "The natives are getting close!" << endl;
+	}
+	if (miles >= 200)
+	{
+		cout << "You have crossed the desert and escaped the natives." << endl;
+		done = true;
+		return;
+	}
+}// end alerts
+
+void actions(char userChoice, int &thirst, int &camelTiredness, int &miles, int &nativesDistance, bool &done, int &canteenDrinks)
+{
+	switch (userChoice)
+	{
+		case 'A':
+		{
+			drinkCanteen(canteenDrinks, thirst);
+		}
+		break;
+		case'a':
+		{
+			drinkCanteen(canteenDrinks, thirst);
+		}
+		break;
+		case 'B':
+		{
+			aheadModerate(thirst, camelTiredness, miles, nativesDistance);
+		}
+		break;
+		case 'b':
+		{
+			aheadModerate(thirst, camelTiredness, miles, nativesDistance);
+		}
+		break;
+		case 'C':
+		{
+			aheadFull(thirst, camelTiredness, miles, nativesDistance);
+		}
+		break;
+		case 'c':
+		{
+			aheadFull(thirst, camelTiredness, miles, nativesDistance);
+		}
+		break;
+		case 'D':
+		{
+			stopForNight(camelTiredness);
+		}
+		break;
+		case 'd':
+		{
+			stopForNight(camelTiredness);
+		}
+		break;
+		case 'E':
+		{
+			statusCheck(canteenDrinks, miles, nativesDistance);
+		}
+		break;
+		case 'e':
+		{
+			statusCheck(canteenDrinks, miles, nativesDistance);
+		}
+		break;
+		case 'Q':
+		{
+			quit(done, userChoice);
+		}
+		break;
+		case 'q':
+		{
+			quit(done, userChoice);
+		}
+	}
+}// end actions
